4534f1b8df1273fe8eba3ca27dcf877e8d4ff179
delberss <delberss@hotmail.com>
2025-02-17 15:42:15 -0300

    separando dados de um JSON e criando vários arquivos JSON

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
 
@@ -24,3 +25,8 @@ if response.status_code == 200:
     print(dados_restaurante['McDonald’s'])  # Exibir o dicionário formatado
 else:
     print(f'Erro ao acessar API. Código: {response.status_code}')
+
+for nome_do_restaurante, dados in dados_restaurante.items():
+    nome_do_arquivo = f'{nome_do_restaurante}.json'
+    with open(nome_do_arquivo, 'w') as arquivo_restaurante:
+        json.dump(dados, arquivo_restaurante, indent=4)
